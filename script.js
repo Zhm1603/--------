fea6b7ceedc1d2e2f218249fa77b74a5e39eccaf
Zhm1603 <zhanel.marat2006@gmail.com>
2024-11-26 23:13:01 +0600

    test

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function changeMenu(){
+    let burger = document.querySelector('.burger');
+    burger.classList.toggle('active');
+    let nav = document.querySelector('.nav-bar');
+    nav.classList.toggle('open')
+}
+
